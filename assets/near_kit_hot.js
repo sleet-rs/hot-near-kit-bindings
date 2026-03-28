bbdf9b963cfd47720a698df26d1ffdcd872789e0
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-03-28 11:10:41 -0500

    Update near_kit_hot.js

--- assets/near_kit_hot.js
+++ assets/near_kit_hot.js
@@ -1,5 +1,5 @@
 import {
-  near_kit_client,
+  // near_kit_client,
   near_connect_client,
 } from "https://esm.sh/@sleet-js/near-kit-custom-client-setup-with-other-custom-functions@0.0.8";
 // ================================================
